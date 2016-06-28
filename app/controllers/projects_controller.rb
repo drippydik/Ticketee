9945bb66d4c069de313c222ad9dda2b3c74b0a4b
drippydik <drippydik@gmail.com>
2016-06-28 20:12:38 +0100

    Redirect the users back to the project page if they try going to a project that does not exist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update, :destroy]
     def index
       @projects = Project.all
     end
@@ -18,16 +19,8 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def show
-    @project = Project.find(params[:id])
-  end
-
-  def edit
-    @project = Project.find(params[:id])
-  end
-
   def update
-    @project = Project.find(params[:id])
+
 
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
@@ -39,7 +32,7 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
+
     @project.destroy
 
     flash[:notice] = "Project has been deleted."
@@ -47,6 +40,14 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
+
 
   def project_params
     params.require(:project).permit(:name, :description)
