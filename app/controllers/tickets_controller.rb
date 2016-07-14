65bfa1407b0d33111b0ef128cf10669ba18b34ed
drippydik <drippydik@gmail.com>
2016-07-14 20:50:40 +0100

    Added Ticket editing

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -17,6 +17,16 @@ class TicketsController < ApplicationController
   end
   def show
   end
+  def edit
+  end
+  def update
+    if @ticket.update(ticket_params)
+          flash[:notice] = "Ticket has been updated."
+          redirect_to [@project, @ticket]
+    else flash.now[:alert] = "Ticket has not been updated."
+        render "edit"
+    end
+  end
   private
   def ticket_params
     params.require(:ticket).permit(:name, :description)
